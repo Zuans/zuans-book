ce6c485ef7644e7c6d290a06f56498c2cd05e682
Zuans <juandev.net@gmail.com>
2020-11-15 04:56:12 +0700

    fixing 2

--- server/src/db.js
+++ server/src/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async (key,name) => {
-    console.log(key,name);
+    console.log(key,name,'ini di function');
     try {
         await mongoose.connect(`mongodb+srv://juan123:${key}@cluster0.npqqt.mongodb.net/${name}?retryWrites=true&w=majority`,{
             useNewUrlParser : true,
